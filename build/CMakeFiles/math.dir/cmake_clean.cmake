1a20c1777017773946a702cdbb89c97f5eb9990e
braydyn <braydynd04@gmail.com>
2024-09-06 22:54:10 -0400

    build

---
+++ build/CMakeFiles/math.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/math.dir/src/main.cpp.obj"
+  "CMakeFiles/math.dir/src/main.cpp.obj.d"
+  "libmath.dll.a"
+  "math.exe"
+  "math.exe.manifest"
+  "math.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
